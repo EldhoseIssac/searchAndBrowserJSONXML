3c4fc60a2e3e3cb317ed1d6f89a5cc7223f89f4d
Eldhose <isletsys@islet21.islet.net>
2012-10-11 16:32:54 +0530

    Initial Commit

---
+++ searchAndBrowserJSONXML/AppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.h
+//  searchAndBrowserJSONXML
+//
+//  Created by isletsystems on 11/10/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:(id)sender;
+
+@end
---
+++ searchAndBrowserJSONXML/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  searchAndBrowserJSONXML
+//
+//  Created by isletsystems on 11/10/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
